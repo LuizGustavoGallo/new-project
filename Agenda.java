03faa0276edb75c3cf0c10dbe6b51d937688f974
Gustavo <l.gustavo.gallo20@gmail.com>
2025-08-04 15:50:36 -0300

    Importação e criação de uma ArrayList e Scanner

--- Agenda.java
+++ Agenda.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Agenda {
 
     public static void main(String[] args) {
 
-        System.out.println("Olá");
+       System.out.println("Bem vindo a sua Agenda de contatos."); //Apresentação do Programa
+
+       ArrayList<String> nomes_contatos = new ArrayList<>(); // criação da Lista que será usada para adicionar os contatos.
+
+       Scanner leitor = new Scanner(System.in); // criação do Scanner para leitura da entrada do usuario
+
+
+
+
+
+       leitor.close(); // fechamento do Scanner.
     }
 }
