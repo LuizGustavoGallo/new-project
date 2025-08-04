d9d148ea10efb9c330b71b80fe0a469a32301749
Gustavo <l.gustavo.gallo20@gmail.com>
2025-08-04 16:41:24 -0300

    Criação do menu interativo

--- Agenda.java
+++ Agenda.java
@@ -8,10 +8,21 @@ public class Agenda {
        System.out.println("Bem vindo a sua Agenda de contatos."); //Apresentação do Programa
 
        ArrayList<String> nomes_contatos = new ArrayList<>(); // criação da Lista que será usada para adicionar os contatos.
-
+       
        Scanner leitor = new Scanner(System.in); // criação do Scanner para leitura da entrada do usuario
 
+        do {
+            // Exibição do menu: 
+            System.out.println("Escolha uma das opções: ");
+            System.out.println("[1] Adicionar contato: ");
+            System.out.println("[2] Editar contato: ");
+            System.out.println("[3] Encontrar contato:");
+            System.out.println("[4] Excluir contato: ");
+            System.out.println("[5] Sair da Agenda.");
 
+            int opcao_Escolhida = leitor.nextInt();  //variavel para armazenar a opção escolhida pelo usuario
+
+        } while ();
 
 
 
